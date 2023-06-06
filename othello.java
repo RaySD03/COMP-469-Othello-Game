376b9005dfb2624fdf904826404768578f78f7ec
RaySD03 <113494325+RaySD03@users.noreply.github.com>
2023-06-05 20:12:12 -0700

    Add files via upload

---
+++ othello.java
@@ -0,0 +1,208 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Panel;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.ObjectInputFilter.Status;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class othello extends JPanel {
+    private static int size = 8;
+    private static int icon_length = 55;
+    private static final Color backgroundColor = Color.BLACK;     
+    private static final Color cellColor = Color.GREEN.darker();    //Board Cell Color
+    private static JLabel[][] labelGrid = new JLabel[size][size];   //Represent Cells with 8 x 8 Jlabels
+    private static Icon blankIcon;
+    private static Icon blackIcon;
+    private static Icon whiteIcon;
+    static JLabel Status = new JLabel("Status: Black begins");
+    static JLabel blackCount = new JLabel("Black: 30");
+    static JLabel whiteCount = new JLabel("White: 30");
+    static JButton resetButton = new JButton("Start Over");
+
+    //Gameplay variables
+    private static String player = "black"; 
+    private static int blackDiscs = 30;
+    private static int whiteDiscs = 30;
+    private static int[][] boardMatrix = new int[8][8]; // 0 = blank, 1 = black, 2 = white
+
+    public othello() {
+        blankIcon = createIcon(new Color(0,0,0,0));
+        blackIcon = createIcon(Color.BLACK);
+        whiteIcon = createIcon(Color.WHITE);
+
+        setBackground(backgroundColor);
+        setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        setLayout(new GridLayout(size, size, 1, 1));
+
+        MyMouse myMouse = new MyMouse();
+
+        // Clickable JLables 
+        for (int i = 0; i < labelGrid.length; i++) {
+            for (int j = 0; j <labelGrid[i].length; j++) {
+                JLabel label = new JLabel(blankIcon);
+                label.setOpaque(true);
+                label.setBackground(cellColor);
+                label.addMouseListener(myMouse);
+                labelGrid[i][j] = label;
+                add(label);
+            }
+        }
+    }
+
+    private Icon createIcon(Color color) {
+        BufferedImage img = new BufferedImage(icon_length, icon_length, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = img.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setColor(color);
+        int gap = 4;
+        int width = icon_length - 2 * gap;
+        int height = width;
+        g.fillOval(gap, gap, width, height); 
+        g.dispose();
+        return new ImageIcon(img);
+    }
+
+    public static void placeDisc(String color,int x,int y) {
+        
+        if (color == "black") {
+            labelGrid[x][y].setIcon(blackIcon);
+            othello.boardMatrix[x][y] = 1;
+        }
+        else {
+            labelGrid[x][y].setIcon(whiteIcon);
+            othello.boardMatrix[x][y] = 2;
+        }
+    }
+
+    private class MyMouse extends MouseAdapter {
+        @Override
+        public void mousePressed(MouseEvent e) {
+           
+            JLabel label = (JLabel) e.getSource();    
+
+            // Detect Which Cell Was Clicked
+            int x = -1;
+            int y = -1;
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (label == labelGrid[i][j]) {
+                        x = j;
+                        y = i;
+                        break;
+                    }
+                }
+                if (x >= 0) {
+                    break;
+                }
+            }
+            
+            if (x >= 0) {
+                // Check if cell is empty or not
+                Icon icon = label.getIcon();
+          
+                if (player == "black" && icon == blankIcon && blackDiscs != 0) {
+                    label.setIcon(blackIcon);
+                    blackDiscs --;
+                    Status.setText("Status: White's turn.");
+                    blackCount.setText("Black: " + blackDiscs);
+                    System.out.printf("Jlabel[%d][%d] is " + player + "%n",x,y);
+                    othello.boardMatrix[x][y] = 1;
+                    player = "white";
+                    othello.printBoard();
+                }
+                else if (player == "white" && icon == blankIcon && whiteDiscs != 0) {
+                    label.setIcon(whiteIcon);
+                    whiteDiscs --;
+                    Status.setText("Status: Black's turn");
+                    whiteCount.setText("White: " + whiteDiscs);
+                    System.out.printf("Jlabel[%d][%d] is " + player + "%n",x,y);
+                    othello.boardMatrix[x][y] = 2;
+                    player = "black";  
+                    othello.printBoard();
+                }
+           
+            }
+        }
+    }
+
+    static othello mainPanel = new othello();
+
+    private static void printBoard() {
+      
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(" " + mainPanel.boardMatrix[j][i]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void setupGame() {
+   
+        //Initialize the boardMatrix with 0s
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                boardMatrix[j][i] = 0;
+                labelGrid[j][i].setIcon(blankIcon);        
+            }
+        }
+        whiteDiscs = 30;
+        blackDiscs = 30;
+        player = "black";
+
+        //Initialize the board with 4 discs
+        mainPanel.placeDisc("black", 3,3);
+        mainPanel.placeDisc("white", 3,4);
+        mainPanel.placeDisc("black", 4,4);
+        mainPanel.placeDisc("white", 4,3);
+
+        blackCount.setText("Black: " + blackDiscs);
+        whiteCount.setText("White: " + whiteDiscs);
+        Status.setText("Status: Black begins");
+    }
+
+    private static void setupGUI() {
+        setupGame();
+
+        //Create the game window
+        JFrame mainFrame = new JFrame("Othello");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.getContentPane().add(mainPanel);
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+
+        //Add Status Bar
+        JToolBar Stats = new JToolBar();
+        Stats.setFloatable(false);
+        
+        Stats.add(resetButton);
+        Stats.addSeparator();
+        Stats.add(Status);
+        Stats.addSeparator();
+        Stats.add(blackCount);
+        Stats.addSeparator();
+        Stats.add(whiteCount);
+        mainFrame.add(Stats, BorderLayout.PAGE_START);
+
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> setupGUI());  
+        
+        //MouseEvent for reseting game
+        resetButton.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("Restart Game.");
+                setupGame();
+            }
+        });
+    }
+}
